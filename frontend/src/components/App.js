6497af240b66ba29ff86b612abe10ecf87b56da0
Frank Hamand <frankhamand@gmail.com>
2018-08-10 16:32:50 +0100

    Add drag+drop

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import RetroColumn from './RetroColumn';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 import './App.css'
 import "antd/dist/antd.css";
@@ -11,11 +12,15 @@ class App extends Component {
   <div className="App">
     <h1>Retrospective</h1>
 
-    <RetroColumn title="Actions" colour="Red" layout="vertical" cardWidth="200px"/>
+    <DragDropContext>
+      <RetroColumn title="Actions" colour="Red" layout="vertical" cardWidth="200px"/>
+    </DragDropContext>
     <div style={{display: "flex"}}>
-      <RetroColumn title="Positive" colour="lime"/>
-      <RetroColumn title="Mixed" colour="orange"/>
-      <RetroColumn title="Negative" colour="maroon"/>
+      <DragDropContext>
+        <RetroColumn title="Positive" colour="lime"/>
+        <RetroColumn title="Mixed" colour="orange"/>
+        <RetroColumn title="Negative" colour="maroon"/>
+      </DragDropContext>
     </div>
   </div>
 )
--- frontend/src/components/RetroColumn.js
+++ frontend/src/components/RetroColumn.js
@@ -1,33 +1,61 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import { List, Card } from 'antd';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
 
 class RetroColumn extends Component {
   render() {
     const data = [
       {
+        id: 1,
         message: "Retro message",
+        index: 1,
+      },
+      {
+        id: 2,
+        message: "Retro message",
+        index: 2,
       }
     ]
 
     const { title, colour, layout, cardWidth } = this.props;
 
     return (
-      <List
-        style={{ flex: 1 }}
-        header={<h2>{title}</h2>}
-        itemLayout={layout || "horizontal"}
-        dataSource={data}
-        renderItem={item => (
-          <List.Item>
-            <Card style={{ width: (cardWidth || "100%"), backgroundColor: colour }}>
-              <p>
-                {item.message}
-              </p>
-            </Card>
-          </List.Item>
+      <Droppable droppableId={title}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            style={{ flex: 1 }}
+          >
+            <List
+              header={<h2>{title}</h2>}
+              itemLayout={layout || "horizontal"}
+              dataSource={data}
+              renderItem={item => (
+                <Draggable key={`${title}-${item.id}`} draggableId={`${title}-${item.id}`} index={item.index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <List.Item>
+                        <Card
+                            style={{ width: (cardWidth || "100%"), backgroundColor: colour }}>
+                          <p>
+                            {item.message}
+                            {item.id}
+                          </p>
+                        </Card>
+                      </List.Item>
+                    </div>
+                  )}
+                </Draggable>
+              )}
+            />
+          </div>
         )}
-      />
+      </Droppable>
     )
   }
 }
