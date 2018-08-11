449a2c0bbabbf05cc2dc80336a1157c7856c1d31
Frank Hamand <frankhamand@gmail.com>
2018-08-11 09:54:09 +0100

    Add new retrospective endpoint

--- cmd/rocketboard/main.go
+++ cmd/rocketboard/main.go
@@ -6,12 +6,24 @@ import (
 	"github.com/arachnys/rocketboard/cmd/rocketboard/repository/inmem"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	svc := NewRocketboardService(inmem.NewRepository())
 
 	http.Handle("/query-playground", handler.Playground("Rocketboard", "/query"))
+	http.HandleFunc("/retrospective/new", func(w http.ResponseWriter, r *http.Request) {
+		name := "Retrospective " + time.Now().Format("2006-01-02")
+		id, err := svc.StartRetrospective(name)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		http.Redirect(w, r, "/"+id, http.StatusFound)
+	})
+
 	http.Handle("/query", handler.GraphQL(
 		graph.NewExecutableSchema(graph.Config{
 			Resolvers: graph.NewResolver(svc),
