b54cedf113c0c64be4ef7c0b0a36e14da51cdecf
Frank Hamand <frankhamand@gmail.com>
2018-08-20 22:21:01 +0100

    Fix incrementing count in service vote
    We don't actually use the count in the sql repository, but we need
    to increment it for the response to the frontend

--- cmd/rocketboard/service.go
+++ cmd/rocketboard/service.go
@@ -74,10 +74,11 @@ func (s *rocketboardService) NewVote(cardId string, voter string) (*model.Vote, 
 			Updated: time.Now(),
 			CardId:  cardId,
 			Voter:   voter,
-			Count:   1,
+			Count:   0,
 		}
 	}
 
+	vote.Count += 1
 	vote.Updated = time.Now()
 	err = s.db.NewVote(vote)
 	return vote, err
